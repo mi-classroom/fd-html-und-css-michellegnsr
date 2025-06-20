c5de06014f56d154f85c14ac4c6f077424a2eb35
Michelle Eigenseer <michelle.eigenseer@smail.th-koeln.de>
2025-06-20 14:36:31 +0200

    OPN dynamisch (ohne ref bisher)

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
+
+
 /* Menü soll sich öffnen */
 const menuOpenButton = document.querySelector('[data-js-main-menu-open]');
 const menuOverlay = document.querySelector('[data-js-main-menu-target]');
+
 menuOpenButton.addEventListener('click', function(){
     menuOverlay.classList.add('is-active');
 })
@@ -17,3 +20,20 @@ menuCloseButton.addEventListener('click', function() {
     menuOverlay.classList.remove('is-active');
 });
 
+
+/* Navigation dynamisch generieren */
+const pageNavigation = document.querySelector('[data-js-page-navigation-menu]');
+console.log(pageNavigation);
+
+const sectionHeadlines = document.querySelectorAll('[data-js-page-navigation-content] > section > h2');
+sectionHeadlines.forEach(function(headline) {
+
+    const sectionID = headline.innerText.toLowerCase().replaceAll(' ', '-');
+
+    const pageNavigationItem = document.createElement('li');
+    const pageNavigationAnchor = document.createElement('a');
+
+    pageNavigationAnchor.innerHTML = headline.innerHTML;
+    pageNavigationItem.appendChild(pageNavigationAnchor);
+    pageNavigation.appendChild(pageNavigationItem);
+});
